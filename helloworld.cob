e3a64386e311739cbb9da1d60a145f5931df2fbe
Alexander Moriarty <alexander@dal.ca>
2013-03-31 20:40:40 +0200

    COBOL is annoying. Added Comment with instructions for includes and lib

--- helloworld.cob
+++ helloworld.cob
@@ -2,7 +2,7 @@
 000101* COBOL is annoying. 
 000102* Compiled with OpenCobol, insalled with homebrew via boxen
 000103* export LIBRARY_PATH=/opt/boxen/homebrew/lib
-000104* export C_LIBRARY_PATH=/opt/boxen/homebrew/lib
+000104* export C_INCLUDE_PATH=/opt/boxen/homebrew/include
 000200 identification division.
 000300 program-id. hello.
 000400 procedure division.
