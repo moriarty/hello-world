1b103db11e55150ad7fcc4a4d06097c78f300f8a
Jacob Perron <jacobmperron@gmail.com>
2013-07-22 19:12:24 +0300

    Moved into function

--- helloworld.lua
+++ helloworld.lua
@@ -1,2 +1,10 @@
---Hello World in Lua
-print('Hello World!')
+#!/usr/bin/env lua
+
+--Hello World program in Lua
+function main(...)
+	print('Hello World!')
+end
+
+if arg ~= nil and arg[-1] ~= nil then
+	main(...)
+end
